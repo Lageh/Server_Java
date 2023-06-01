01ff24a6758c26e1281c894c12fcebb9a38c9a79
Guilherme Guerra <guilherme0895@gmail.com>
2023-06-01 14:53:42 -0300

    Added generate testRequest to validate HTTP protocol

---
+++ src/test/com/gui/httpserver/http/HttpParserTest.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2023. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package com.gui.httpserver.http;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class HttpParserTest {
+
+    private HttpParser httpParser;
+    @BeforeAll
+    public void beforeCass() {
+        httpParser = new HttpParser();
+    }
+
+    @Test
+    void parseHttpRequest () {
+        httpParser.parseHttpRequest(
+                generateValidTestCase()
+        );
+    }
+
+    private InputStream generateValidTestCase(){
+        String rawData = "GET / HTTP/1.1\r\n" +
+                "Host: localhost:6060\r\n" +
+                "Connection: keep-alive\r\n" +
+                "sec-ch-ua: \"Google Chrome\";v=\"113\", \"Chromium\";v=\"113\", \"Not-A.Brand\";v=\"24\"\r\n" +
+                "sec-ch-ua-mobile: ?0\r\n" +
+                "sec-ch-ua-platform: \"Windows\"\r\n" +
+                "Upgrade-Insecure-Requests: 1\r\n" +
+                "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36\r\n" +
+                "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7\r\n" +
+                "Sec-Fetch-Site: none\r\n" +
+                "Sec-Fetch-Mode: navigate\r\n" +
+                "Sec-Fetch-User: ?1\r\n" +
+                "Sec-Fetch-Dest: document\r\n" +
+                "Accept-Encoding: gzip, deflate, br\r\n" +
+                "Accept-Language: en-US,en;q=0.9,pt-BR;q=0.8,pt;q=0.7,es-419;q=0.6,es;q=0.5" +
+                "\r\n";
+
+
+        return new ByteArrayInputStream(
+                rawData.getBytes(
+                        StandardCharsets.US_ASCII));
+    }
+}
